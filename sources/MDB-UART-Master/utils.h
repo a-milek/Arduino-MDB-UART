b7c557d8f865f620f8d4cce8105ceb7239c6f29d
Wojciech Milek <wmilek@users.noreply.github.com>
2025-12-31 15:34:08 +0100

    ARRAY_SIZE and other zephyr tools

--- sources/MDB-UART-Master/utils.h
+++ sources/MDB-UART-Master/utils.h
@@ -6,4 +6,14 @@
 
 uint16_t BCDByteToInt(uint8_t* BCDBytes, size_t BCDBytes_size);
 
+#define ZERO_OR_COMPILE_ERROR(cond) ((int) sizeof(char[1 - 2 * !(cond)]) - 1)
+ 
+#define IS_ARRAY(array) \
+        ZERO_OR_COMPILE_ERROR( \
+                !__builtin_types_compatible_p(__typeof__(array), \
+                                              __typeof__(&(array)[0])))
+
+#define ARRAY_SIZE(array) \
+        ((long) (IS_ARRAY(array) + (sizeof(array) / sizeof((array)[0]))))
+
 #endif
